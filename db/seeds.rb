c27f26b405f4cf3b5ecc29f4108ef81f574b547e
Nikolay <Xrazik@yandex.ru>
2019-11-11 18:00:39 +0300

    Added default values to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(title: 'Frontend')
+Category.create(title: 'Backend')
+
+Test.create(title: 'Ruby', level: 1, category_id: 2)
+Test.create(title: 'Python', level: 0, category_id: 2)
+Test.create(title: 'HTML', level: 0, category_id: 1)
+
+User.create(name: 'Иван', surname: 'Иванов', email: 'ivan@mail.ru',
+            password: '12345', privilege: 'user', test_ids: '2,3')
