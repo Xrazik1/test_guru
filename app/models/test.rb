49aa9b6ebb183157dc8702f890b2220806094d5d
Nikolay <Xrazik@yandex.ru>
2019-11-11 21:00:24 +0300

    Added to many relationship

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  has_and_belongs_to_many :users
+
   def self.get_names_by_cat(cat_name)
     category = Category.where(title: cat_name)
     where(category_id: category.first.id).order(id: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
+
   def tests_by_level(level)
     ids = test_ids.split(',').map(&:to_i)
     Test.where('id IN (?) AND level = ?', ids, level)
