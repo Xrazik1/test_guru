73a9f718a1244c86f57b9b7ec0330abf8c977f3f
Nikolay <Xrazik@yandex.ru>
2019-11-11 21:00:49 +0300

    tests_by_level method fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
 
   def tests_by_level(level)
-    ids = test_ids.split(',').map(&:to_i)
-    Test.where('id IN (?) AND level = ?', ids, level)
+    tests.where('level = ?', level)
   end
 end
