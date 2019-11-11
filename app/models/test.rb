f1de1dea9470bceba92cbf54bef9380b90afc9f8
Nikolay <Xrazik@yandex.ru>
2019-11-11 18:01:52 +0300

    Added getting names of tests by category from Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  def self.get_names_by_cat(cat_name)
+    category = Category.where(title: cat_name)
+    where(category_id: category.first.id).order(id: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  def tests(level)
+  def tests_by_level(level)
     ids = test_ids.split(',').map(&:to_i)
     Test.where('id IN (?) AND level = ?', ids, level)
   end
