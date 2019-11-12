7085a0d46ff3db01fea7dd9af1db213ab607f292
Nikolay <Xrazik@yandex.ru>
2019-11-13 02:57:00 +0300

    Added connection between categories and tests, tests and questions

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,8 @@
 
 class Test < ApplicationRecord
   has_and_belongs_to_many :users
+  has_many :questions
+  belongs_to :category
 
   def self.get_names_by_cat(cat_name)
     joins('JOIN categories ON categories.id = tests.category_id')
