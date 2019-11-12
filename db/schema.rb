1a681c444003c97ba768e5e46113be0dc9b72570
Nikolay <Xrazik@yandex.ru>
2019-11-13 02:55:30 +0300

    Fixed indexes of join table users tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_173201) do
+ActiveRecord::Schema.define(version: 2019_11_12_233333) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -47,8 +47,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_173201) do
   create_table "tests_users", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
-    t.index ["test_id", "user_id"], name: "index_tests_users_on_test_id_and_user_id"
-    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id"
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
