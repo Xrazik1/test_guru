763f1a5ebca2b18048bea658e1b947a55d7cd1f2
Nikolay <Xrazik@yandex.ru>
2019-11-16 01:43:40 +0300

    Fixed get_names_by_cat method

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,17 +9,18 @@ class Test < ApplicationRecord
 
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
-  scope :hard, -> { where(level: 4..Float::INFINITY) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
 
-  scope :get_names_by_cat, lambda { |cat_name|
-    joins('JOIN categories ON categories.id = tests.category_id')
-      .where('categories.title = ?', cat_name)
-      .order(id: :desc)
-      .pluck(:title)
+  scope :by_cat_name, lambda { |cat_name|
+    joins('JOIN categories ON categories.id = tests.category_id').where('categories.title = ?', cat_name)
   }
 
-  validates :title, presence: true, uniqueness: true
-  validates :level, uniqueness: true
+  def self.get_names_by_cat(cat_name)
+    by_cat_name(cat_name).order(id: :desc).pluck(:title)
+  end
+
+  validates :title, presence: true
+  validate :validate_match_level_and_title
   validates_numericality_of :level, greater_than_or_equal_to: 0
 
 end
