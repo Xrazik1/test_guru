da706a9c295093421e377833dcfb0673a4bd2783
Nikolay <Xrazik@yandex.ru>
2019-11-16 01:43:05 +0300

    Fixed correct scope and answers count validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,7 +5,7 @@ class Answer < ApplicationRecord
 
   belongs_to :question
 
-  scope :correct, ->(question_id) { where(correct: 'true', question_id: question_id) }
+  scope :correct, -> { where(correct: 'true') }
 
   validates :body, presence: true
   validate :validate_answers_count, on: :create
@@ -13,8 +13,8 @@ class Answer < ApplicationRecord
   private
 
   def validate_answers_count
-    if self.class.select(:id).where(question_id: question_id).count >= ANSWERS_MAX_COUNT
-      errors.add :answer, 'Вопрос достиг максимального числа ответов'
+    if question.answers.count >= ANSWERS_MAX_COUNT
+      errors.add :message, "'Вопрос достиг максимального числа ответов'"
     end
   end
 end
