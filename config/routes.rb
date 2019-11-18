f40ef482f1137331c9c0aa8ebc29f2fcd5edee6b
Nikolay <Xrazik@yandex.ru>
2019-11-19 02:05:01 +0300

    chore(Router): removed useless resource, added shallow option

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tests
-
   resources :tests do
-    resources :questions
+    resources :questions, shallow: true
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
