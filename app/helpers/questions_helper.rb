0d73cf87c605abc99c02374aa5fb6576c2195705
Nikolay <Xrazik@yandex.ru>
2019-11-21 07:18:59 +0300

    feat(Helpers): added helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
+  def question_header(entity, mode)
+    header = "Создать новый '#{entity.title}' вопрос" if mode == 'new'
+    header = "Изменить '#{entity.title}' вопрос" if mode == 'edit'
+    header
+  end
 end
