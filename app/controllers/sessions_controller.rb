7043e0e12f44c6ba8c4fb4eac1d7c628781d9b3d
Nikolay <Xrazik@yandex.ru>
2019-11-28 20:40:27 +0300

    feat(SessionsController): added logging out when user redirects to login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new
-    
-  end
+  before_action :log_out!, only: :new
 
   def create
     user = User.find_by(email: params[:email])
