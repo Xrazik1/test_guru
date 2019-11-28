b62470a14eedc58e50ada2ece49fb49d0163461b
Nikolay <Xrazik@yandex.ru>
2019-11-28 20:33:37 +0300

    chore(UserModel): added uniqueness and format checking of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/.freeze
+
   has_many :author_tests, class_name: 'Test', foreign_key: 'author_id'
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEXP }
+  validates_uniqueness_of :email
 
   has_secure_password
 
