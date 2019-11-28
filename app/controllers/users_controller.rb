e0419a3d4892b977cdf54b9f45caf919188cbb86
Nikolay <Xrazik@yandex.ru>
2019-11-28 20:35:01 +0300

    chore(UserController): added user_id saving to session after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save ? redirect_to(tests_path) : render(:new)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to(tests_path)
+    else
+      render(:new)
+    end
   end
 
   private
