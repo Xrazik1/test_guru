55734637fa833c618901a860cd2630c65866f599
Nikolay <Xrazik@yandex.ru>
2019-12-03 19:27:23 +0300

    feat(App): added and configured mailer

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,7 +3,6 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_test, only: %i[show start]
-  before_action :find_user, only: :start
 
   def index
     @tests = Test.all
@@ -12,17 +11,13 @@ class TestsController < ApplicationController
   def show; end
 
   def start
-    @user.tests << @test
-    redirect_to @user.test_passage(@test)
+    current_user.tests << @test
+    redirect_to current_user.test_passage(@test)
   end
 
   private
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def find_user
-    @user = current_user
   end
 end
