12e6336ed268055669130aef5522124a3ed2f897
Nikolay <Xrazik@yandex.ru>
2019-12-05 01:03:16 +0300

    chore(ApplicationController): added redirecting to admin root after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if current_user.privilege == 'admin'
+      admin_tests_path
+    else
+      root_path
+    end
+  end
 end
