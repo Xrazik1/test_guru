87b793c8d918a7c4cbf5329d790d5e2ae9033a1b
Nikolay <Xrazik@yandex.ru>
2019-12-12 16:11:27 +0300

    chore(Gists): token moved to env, added dotenv

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -3,7 +3,6 @@
 class GitHubClient
 
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = 'b40a73787a49571372c6d202c9f0372dae3a1890'
 
   def initialize
     @http_client = setup_http_client
