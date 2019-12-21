93abf9a7cfca207ebdad17506a9861939221f900
Nikolay <Xrazik@yandex.ru>
2019-12-21 23:12:25 +0300

    fix(ActionMailer): fixed incorrect name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'test-guru-thinknetica.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_options = {
+  config.action_mailer.smtp_settings = {
     adress: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
