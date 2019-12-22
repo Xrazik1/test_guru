dcb75e66f8a3b61a0b5b06f88fae430cd7b1fbf9
Nikolay <Xrazik@yandex.ru>
2019-12-22 04:11:34 +0300

    fix(ProductionInitializer): added domain to smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     adress: 'smtp.gmail.com',
     port: 587,
+    domain: 'gmail.com',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
