8bf12cac6b92c77f40480ddc706bd82d9d91845d
Nikolay <Xrazik@yandex.ru>
2019-12-22 04:25:51 +0300

    fix(ProductionInitializer): added protocol to host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'test-guru-thinknetica.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://test-guru-thinknetica.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     adress: 'smtp.gmail.com',
