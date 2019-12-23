1d38985b7cab1ce8e9bd23c4a131d36447811a11
Nikolay <Xrazik@yandex.ru>
2019-12-23 17:28:08 +0300

    fix(ProductionEnviroment): fixed mail.ru domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'test-guru-thinknetica.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mail.ru',
+    address: 'tls://smtp.mail.ru',
     port: 465,
     domain: 'test-guru-thinknetica.herokuapp.com',
     user_name: ENV['SMTP_USERNAME'],
