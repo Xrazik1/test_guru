eca8b40a449d574f4d96be28b8be1a40e7bf0b2b
Nikolay <Xrazik@yandex.ru>
2019-12-23 17:10:15 +0300

    chore(ProductionEnviroment): gmail changed to mail.ru

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'test-guru-thinknetica.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
+    address: 'smtp.mail.ru',
+    port: 465,
     domain: 'test-guru-thinknetica.herokuapp.com',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
